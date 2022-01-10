a4ba319323a309b486f81a5d5b332325068099b6
rafiffuaddo <rafiffuad3108@gmail.com>
2022-01-10 23:31:52 +0700

    Add project files.

---
+++ ServerChatApp_20190140129_RafifFuaddoshofha/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFService_2Way_20190140129;
+
+
+namespace ServerChatApp_20190140129_RafifFuaddoshofha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
